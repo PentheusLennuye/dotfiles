6e6c86c60abc5ef255deb1802a6b2dad9da3d854
PentheusLennuye <gmc@cummings-online.ca>
2024-03-19 10:54:48 -0400

    Hyprland fix and SDDM on

--- nixos/hosts/sisyphus/runlevel5.nix
+++ nixos/hosts/sisyphus/runlevel5.nix
@@ -7,16 +7,17 @@
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
+
   programs.hyprland = {
     enable = true;
-    # enableNvidiaPatches = true;  # No effect in branch unstable
     xwayland.enable = true;
   };
   programs.xwayland.enable = true;
 
   security.polkit.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
 
   services.desktopManager.plasma6.enable = true;
+  services.xserver.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
