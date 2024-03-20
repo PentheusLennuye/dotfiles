fa0c06d4e7cdab853b55ce83a12470f6f1567f9a
PentheusLennuye <gmc@cummings-online.ca>
2024-03-19 21:32:16 -0400

    Sorted out file manager and added tabs

--- nixos/users/gmc/nvim/lua/plug.lua
+++ nixos/users/gmc/nvim/lua/plug.lua
@@ -5,7 +5,13 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
+-- File Manager
 Plug('preservim/nerdtree', { ['on'] = 'NERDTreeToggle' })
+
+-- Tabs
+Plug('nvim-tree/nvim-web-devicons')
+Plug('lewis6991/gitsigns.nvim')
+Plug('romgrk/barbar.nvim')
 
 vim.call('plug#end')
 
