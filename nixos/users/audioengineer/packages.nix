dea4890b59e03df978fea0bb0fcab93960ea8079
PentheusLennuye <gmc@cummings-online.ca>
2024-04-21 22:39:50 -0400

    Added video streaming obs

--- nixos/users/audioengineer/packages.nix
+++ nixos/users/audioengineer/packages.nix
@@ -5,6 +5,7 @@
     dexed                                  # DX7 VST plugin
     ladspaPlugins                          # Audio effect plugins
     linuxsampler                           # Sample-based synthesizer VST
+    obs-studio                             # Screen recorder
     qsampler                               # GUI for linuxsampler
     qjackctl                               # JACK patch panel for audio work
     qpwgraph                               # Pipewire graph interface
