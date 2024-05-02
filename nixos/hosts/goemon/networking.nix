e080e46c645ccba1b6d29d7f5d5073fd12e27530
PentheusLennuye <gmc@cummings-online.ca>
2024-05-02 11:26:20 -0400

    Added new machine goemon. Welcome goemon

---
+++ nixos/hosts/goemon/networking.nix
@@ -0,0 +1,19 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "goemon";
+  networking.networkmanager.enable = true;
+
+  services.openssh.enable = true;
+  services.openssh.settings = {
+    PasswordAuthentication = true;
+  };
+
+  # hardware.bluetooth.enable = true;
+  # hardware.bluetooth.powerOnBoot = true;
+  # services.blueman.enable = true;
+}
---
+++ nixos/hosts/goemon/opengl.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  systemd.tmpfiles.rules = [
+    "L+ /opt/rocm/hip - - - - ${pkgs.rocmPackages.clr}"
+  ];
+  hardware.opengl = {
+    enable = true;
+    # OpenCL and Vulkan ----------------------
+    extraPackages = with pkgs; [
+      amdvlk
+      rocmPackages.clr.icd
+    ];
+    # Vulkan support -------------------------
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+}
