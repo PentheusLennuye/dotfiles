6c6e4f58ee1852a3f6017e49dd3662d1570d2cc5
PentheusLennuye <gmc@cummings-online.ca>
2024-05-02 11:27:23 -0400

    Added new machine goemon. Welcome goemon

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -40,6 +40,19 @@
           }
         ];
       };
+      goemon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./hosts/goemon
+          ./users.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.gmc = import ./users/gmc/home.nix;
+          }
+        ];
+      };
     };
   };
 }
