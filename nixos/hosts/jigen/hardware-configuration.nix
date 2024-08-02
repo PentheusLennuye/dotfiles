6f10149c60d6797125e5bae096f1c2163055a9df
George Cummings <gmc@jigen.cummings-online.local>
2024-08-02 13:22:47 -0400

    Disable i915 dc

--- nixos/hosts/jigen/hardware-configuration.nix
+++ nixos/hosts/jigen/hardware-configuration.nix
@@ -11,6 +11,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = ["i915.enable_dc=0"];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
