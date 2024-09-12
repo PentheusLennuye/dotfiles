3e6573f1ca15934a5f3d8b07e5a1353c5f326c21
PentheusLennuye <gmc@cummings-online.ca>
2024-09-12 09:29:16 -0400

    K8S without its own lb

--- nixos/roles/k8s.nix
+++ nixos/roles/k8s.nix
@@ -5,6 +5,6 @@ with lib
     services.k3s = {
         enable = true;
         role = "server";
-        extraFalgs = toString [ "--disable service-lb" ]
+        extraFlags = toString [ "--disable service-lb" ];
     };
 }
