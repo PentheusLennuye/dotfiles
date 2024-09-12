f64be061d5527f7a929ff9be9eff2c935e35153c
PentheusLennuye <gmc@cummings-online.ca>
2024-09-12 08:18:49 -0400

    IP stolen by small child

--- services/04_lb/install.sh
+++ services/04_lb/install.sh
@@ -3,5 +3,8 @@
 helm repo add metallb https://metallb.github.io/metallb
 helm -n metallb-system install metallb metallb/metallb --create-namespace
 
+echo "Sleeping for the CRDs to install."
+sleep 30
+
 kubectl apply -f .
 
