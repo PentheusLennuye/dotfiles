db67c8c513856cc5e41b79786ac183277be2d16b
PentheusLennuye <gmc@cummings-online.ca>
2024-12-12 15:25:19 -0500

    Added hibernation to glaucus

--- nixos/system/hosts/glaucus/hibernation.nix
+++ nixos/system/hosts/glaucus/hibernation.nix
@@ -1,10 +1,13 @@
 # Suspend-then-hibernate everywhere
-services.logind = {
-  lidSwitch = "suspend-then-hibernate";
-  extraConfig = ''
-    HandlePowerKey=suspend-then-hibernate
-    IdleAction=suspend-then-hibernate
-    IdleActionSec=2m
-  '';
-};
+{ config, ... }
+{
+  services.logind = {
+    lidSwitch = "suspend-then-hibernate";
+    extraConfig = ''
+      HandlePowerKey=suspend-then-hibernate
+      IdleAction=suspend-then-hibernate
+      IdleActionSec=2m
+    '';
+  };
+}
 systemd.sleep.extraConfig = "HibernateDelaySec=1h";
