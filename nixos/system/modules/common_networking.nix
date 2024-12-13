dc0dc90967f7f7e25465642aca8e1e4de1d246a2
PentheusLennuye <gmc@cummings-online.ca>
2024-12-13 15:22:55 -0500

    Changed networking to local (or vpn) dns

--- nixos/system/modules/common_networking.nix
+++ nixos/system/modules/common_networking.nix
@@ -3,25 +3,7 @@
 {    
   networking.firewall.allowedTCPPorts = [ 22 ];
   networking.firewall.allowPing = true;
-  networking.extraHosts = ''
-    192.168.68.11 zantetsuken.cummings-online.local zantetsuken
-    192.168.68.12 jigen.cummings-online.local jigen
-
-    192.168.68.13 docker.cummings-online.local docker
-    192.168.68.14 portainer.cummings-online.local portainer
-
-    192.168.68.21 ingress.cummings-online.local netbox.cummings-online.local netbox awx.cummings-online.local awx argocd.cummings-online.local
-    192.168.68.31 rpi1.cummings-online.local time.cummings-online.local rpi1 time
-    192.168.68.32 rpi2.cummings-online.local rpi2
-    192.168.68.33 rpi3.cummings-online.local rpi3
-    192.168.68.34 rpi4.cummings-online.local rpi4
-
-    192.168.68.61 mtl-printer.cummings-online.local mtl-printer brother
-
-    192.168.68.73 goemon.cummings-online.local goemon
-    192.168.68.74 goliath.cummings-online.local goliath
-
-  '';
+  networking.nameservers = [ "192.168.68.31" "192.168.68.32" "8.8.8.8" ];
   networking.search = [ "cummings-online.local" "cummings-online.ca" ];
   services.timesyncd = {
     servers = [ "time.cummings-online.local" ];
@@ -30,4 +12,3 @@
     '';
   };
 }
-# [ "0.nixos.pool.ntp.org" "1.nixos.pool.ntp.org" "2.nixos.pool.ntp.org" "3.nixos.pool.ntp.org" ];
