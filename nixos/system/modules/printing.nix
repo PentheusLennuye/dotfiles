24484ad50e16f309670de49574869c12e3b45bc0
PentheusLennuye <gmc@cummings-online.ca>
2025-02-05 15:23:34 -0500

    Brother printer now working

--- nixos/system/modules/printing.nix
+++ nixos/system/modules/printing.nix
@@ -2,14 +2,30 @@
 
 with lib;
 {
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Brother_HL_2370DW";
+        location = "Jean-Brillant";
+        deviceUri = "ipp://mtl-printer/ipp/print";
+        model = "drv:///brlaser.drv/brl2375w.ppd";
+        ppdOptions = {
+          PageSize = "Letter";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "Brother_HL_2370DW";
+  };
   services.printing = {
     enable = true;
     drivers = [ pkgs.brlaser pkgs.canon-cups-ufr2 ];
   };
 
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
+  # Re-enable when Brother printers are no longer needed
+  # services.avahi = {
+  #   enable = true;
+  #   nssmdns4 = true;
+  #   openFirewall = true;
+  # };
+
 }
