3a74df72fa08dffa75b70ea9b86d52e1a0e28630
PentheusLennuye <gmc@cummings-online.ca>
2025-02-21 11:20:35 -0500

    Added hostname to starship

--- nixos/home-manager/gmc/starship.nix
+++ nixos/home-manager/gmc/starship.nix
@@ -6,6 +6,7 @@
   programs.starship.settings = {
     format =  "[░▒▓](#a3aed2)" +
       "[  ](bg:#a3aed2 fg:#090c0c)" +
+      "$hostname" +
       "[](bg:#769ff0 fg:#a3aed2)$directory" +
       "[](fg:#769ff0 bg:#394260)$git_branch$git_status" +
       "[](fg:#394260 bg:#212736)$c$golang$helm$kubernetes$lua$nix_shell$python$rust" +
@@ -46,6 +47,11 @@
       symbol = "⎈";
       style = "bg:#212736";
       format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+    };
+    hostname = {
+      format = "[$hostname]($style)";
+      ssh_only = true;
+      style = "bg:#a3aed2 fg:#090c0c";
     };
     kubernetes = {
       symbol = "⎈";
