a400415092ea616a0a8a0dee8e64ffd9f9ee42ab
PentheusLennuye <gmc@cummings-online.ca>
2025-03-13 22:06:18 -0400

    Added static routes to lupin

--- nixos/system/hosts/goemon/networking.nix
+++ nixos/system/hosts/goemon/networking.nix
@@ -10,9 +10,15 @@
     domain = "cummings-online.local";
     hostName = "goemon";
     interfaces = {
-      enp6s0.ipv4.addresses = [
-        { address = "192.168.68.73"; prefixLength = 24; }
-      ];
+      enp6s0.ipv4 = {
+        addresses = [
+          { address = "192.168.68.73"; prefixLength = 24; }
+        ];
+        routes = [
+          { address = "10.16.73.0"; prefixLength = 24; via = "192.168.68.71"; }
+          { address = "172.16.73.0"; prefixLength = 24; via = "192.168.68.71"; }
+        ];
+      };
     };
   };
 
