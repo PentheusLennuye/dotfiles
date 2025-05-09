cc737cfb56171ec22a0c2e30423de08f66ea8ebb
PentheusLennuye <gmc@cummings-online.ca>
2025-05-09 10:46:08 -0400

    DNS systems for montreal servers set

--- nixos/system/roles/montreal.nix
+++ nixos/system/roles/montreal.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {    
-  networking.nameservers = [ "172.16.73.1" "172.16.73.2" ];
+  networking.nameservers = [ "172.16.73.2" "172.16.73.3" ];
   services.timesyncd = {
     servers = [ "time.cummings-online.local" ];
     extraConfig = ''
