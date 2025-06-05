8f4bbd46f5b6bfed122da2b144f46686ee7b6e66
PentheusLennuye <gmc@cummings-online.ca>
2025-06-04 21:06:34 -0400

    Moved DNS for Montreal again

--- nixos/system/roles/montreal.nix
+++ nixos/system/roles/montreal.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {    
-  networking.nameservers = [ "192.168.68.1" ];
+  networking.nameservers = [ "172.16.73.2" "172.16.73.3" ];
   services.timesyncd = {
     servers = [ "172.16.73.2" ];
     extraConfig = ''
