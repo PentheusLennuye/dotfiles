c40a9f3cda6e75b600f3053fd28909514f74f88e
George Cummings <gmc@cummings-online.ca>
2025-06-08 13:35:26 -0400

    Printers are now added through the CUPS configuration

--- nixos/system/modules/printing.nix
+++ nixos/system/modules/printing.nix
@@ -2,30 +2,37 @@
 
 with lib;
 {
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "Brother_HL_2370DW";
-        location = "Jean-Brillant";
-        deviceUri = "ipp://mtl-printer/ipp/print";
-        model = "drv:///brlaser.drv/brl2370d.ppd";
-        ppdOptions = {
-          PageSize = "Letter";
-        };
-      }
-    ];
-    ensureDefaultPrinter = "Brother_HL_2370DW";
-  };
+  #hardware.printers = {
+  #  ensurePrinters = [
+  #    {
+  #      name = "Brother_HL_2370DW";
+  #      location = "Jean-Brillant";
+  #      deviceUri = "ipp://brother/ipp/print";
+  #     model = "everywhere";
+  #      ppdOptions = {
+  #        PageSize = "Letter";
+  #      };
+  #    }
+  #    {
+  #      name = "Canon_MF_229DW";
+  #      location = "Knowlton";
+  #      deviceUri = "ipp://canon/ipp/print";
+  #      model = "everywhere";
+  #      ppdOptions = {
+  #        PageSize = "Letter";
+  #      };
+  #    }
+  #  ];
+  #  # ensureDefaultPrinter = "Brother_HL_2370DW";
+  #};
   services.printing = {
     enable = true;
-    drivers = [ pkgs.brlaser pkgs.canon-cups-ufr2 ];
   };
 
   # Re-enable when Brother printers are no longer needed
-  # services.avahi = {
-  #   enable = true;
-  #   nssmdns4 = true;
-  #   openFirewall = true;
-  # };
-
+  #services.avahi = {
+  #  enable = true;
+  #  nssmdns4 = true;
+  #  openFirewall = true;
+  #};
 }
