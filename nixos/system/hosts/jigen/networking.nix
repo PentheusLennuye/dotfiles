00c526c56b4f8bfe917c2e7dc150e21cc7b44be8
PentheusLennuye <gmc@cummings-online.ca>
2025-06-09 13:29:38 -0400

    Added all lab to vlan11

--- nixos/system/hosts/jigen/networking.nix
+++ nixos/system/hosts/jigen/networking.nix
@@ -5,12 +5,27 @@
     defaultGateway = "172.16.73.1";
     domain = "cummings-online.local";
     hostName = "jigen";
+
+    # Virtual stuff
+    bridges = {
+      "br1" = {
+        interfaces = [ "enp0s20f0u2" ];
+        rstp = true;
+      };
+    };
+    vlans = {
+      vlan11 = {
+        id = 11;
+        interface = "br1";
+      };
+    };
+
     interfaces = {
       enp2s0.ipv4.addresses = [
         { address = "172.16.73.3"; prefixLength = 24; }
       ];
-      enp0s20f0u2.ipv4.addresses = [
-        { address = "10.11.0.3"; prefixLength = 8; }
+      vlan11.ipv4.addresses = [
+        { address = "10.11.0.3"; prefixLength = 16; }
       ];
     };
     useDHCP = lib.mkDefault false;
--- nixos/system/hosts/lupin/networking.nix
+++ nixos/system/hosts/lupin/networking.nix
@@ -5,15 +5,32 @@
     defaultGateway = "172.16.73.1";
     domain = "cummings-online.local";
     hostName = "lupin";
+    useDHCP = lib.mkDefault false;
+
+    # Virtual stuff
+    bridges = {
+      "br1" = {
+        interfaces = [ "enp0s20f0u10" ];
+        rstp = true;
+      };
+    };
+    vlans = {
+      vlan11 = {
+        id = 11;
+        interface = "br1";
+      };
+    };
+
     interfaces = {
       eno1.ipv4.addresses = [
         { address = "172.16.73.2"; prefixLength = 24; }
       ];
-      enp0s20f0u10.ipv4.addresses = [
-        { address = "10.11.0.2"; prefixLength = 8; }
-      ];
+      vlan11 = {
+        ipv4.addresses = [
+          { address = "10.11.0.2"; prefixLength = 8; }
+        ];
+      };
     };
-    useDHCP = lib.mkDefault false;
   };
   services.openssh = {
     enable = true;
