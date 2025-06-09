98c5f9f5b2a504628b69c6c5a8ac2e62daa6b827
PentheusLennuye <gmc@cummings-online.ca>
2025-06-09 11:37:21 -0400

    Moving VPN network to 10.0.0.0/24

--- nixos/system/roles/vpn-mtl-endpoint.nix
+++ nixos/system/roles/vpn-mtl-endpoint.nix
@@ -9,13 +9,13 @@
       enable = true;
       interfaces = {
         wg0 = {
-          ips = [ "172.16.75.201/24" ];
+          ips = [ "10.0.0.2/24" ];
           listenPort = 51821;
           privateKeyFile = "/usr/local/wireguard/mtl-access.server.key";
           peers = [
             {
               endpoint = "vlb.hopto.me:51821";
-              allowedIPs = [ "192.168.73.0/24" "172.16.75.0/24" ];
+              allowedIPs = [ "192.168.73.0/24" "10.0.0.0/24" ];
               persistentKeepalive = 25;
               publicKey = "PIbyx7f4FQtpzqBwmT04bQuGJCVIp/1kTwTvt6JweDE=";
             }
