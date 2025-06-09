0b41db90ca123ab0cecf848ba41d0a3a6624b060
PentheusLennuye <gmc@cummings-online.ca>
2025-06-09 13:19:48 -0400

    Zenigata going to vlan 11

--- nixos/system/hosts/zenigata/networking.nix
+++ nixos/system/hosts/zenigata/networking.nix
@@ -5,19 +5,38 @@
     defaultGateway = "172.16.73.1";
     domain = "cummings-online.local";
     hostName = "zenigata";
+    useDHCP = lib.mkDefault false;
+
+    # Virtual stuff
+    bridges = {
+      "br1" = {
+        interfaces = [ "enp0s20f0u2" ];
+        rstp = true;
+      };
+    };
+    vlans = {
+      vlan11 = {
+        id = 11;
+        interface = "br1";
+      };
+    };
+
     interfaces = {
       enp2s0.ipv4.addresses = [
         { address = "172.16.73.4"; prefixLength = 24; }
       ];
-      enp0s20f0u2.ipv4.addresses = [
-        { address = "10.11.0.4"; prefixLength = 8; }
-      ];
+      vlan11 = {
+        ipv4.addresses = [{address = "10.11.0.4"; prefixLength = 16;}];
+      };
     };
-    useDHCP = lib.mkDefault false;
   };
 
-  services.openssh.enable = true;
-  services.openssh.settings = {
-    PasswordAuthentication = true;
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = true;
+      };
+    };
   };
 }
