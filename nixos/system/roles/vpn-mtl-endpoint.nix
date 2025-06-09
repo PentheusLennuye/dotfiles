2b209bbe1b0a7209404a14be5ab286a13c3a6b77
PentheusLennuye <gmc@cummings-online.ca>
2025-06-09 12:19:03 -0400

    Transferring road warriors to vpn-mtl-endpoint.nix

--- nixos/system/roles/vpn-mtl-endpoint.nix
+++ nixos/system/roles/vpn-mtl-endpoint.nix
@@ -14,10 +14,26 @@
           privateKeyFile = "/usr/local/wireguard/mtl-access.server.key";
           peers = [
             {
+              # Knowlton S2S
               endpoint = "vlb.hopto.me:51821";
               allowedIPs = [ "192.168.73.0/24" "10.0.0.0/24" ];
               persistentKeepalive = 25;
               publicKey = "PIbyx7f4FQtpzqBwmT04bQuGJCVIp/1kTwTvt6JweDE=";
+            }
+            { # Glaucus Road Warrior
+              allowedIPs = [ "10.0.0.254/24" ];
+              persistentKeepalive = 25;
+              publicKey = "4DUYe0fUEh222uCyO8VMCesTRbwUPwQr4KGnz4m8/FM=";
+            }
+            { # Bunkai Road Warrior
+              allowedIPs = [ "10.0.0.253/24" ];
+              persistentKeepalive = 25;
+              publicKey = "22WG+k6hqPS3nPf0Tk4fJk6Pag9WUnoUMcihCYemaiw=";
+            }
+            { # Murasaki Road Warrior
+              allowedIPs = [ "10.0.0.252/24" ];
+              persistentKeepalive = 25;
+              publicKey = "b+pr0zs+EcKon9GmEoKnPWM8/IzOTupe2ABXRD62/w4=";
             }
           ];
         };
