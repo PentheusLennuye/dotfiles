80215fe34aefd5f948f5a2dd0e58a08177b81cee
PentheusLennuye <gmc@cummings-online.ca>
2025-10-01 20:50:31 -0400

    Added engineering for 3d printing

--- nixos/system/flake.nix
+++ nixos/system/flake.nix
@@ -56,6 +56,7 @@
           ./roles/audio-engineering.nix
           ./roles/container-host.nix
           ./roles/development.nix
+          ./roles/engineering.nix
           ./roles/gaming.nix
           #./roles/knowlton.nix
           ./roles/media.nix
@@ -101,6 +102,7 @@
           ./roles/audio-engineering.nix
           ./roles/container-host.nix
           ./roles/development.nix
+          ./roles/engineering.nix
           ./roles/gaming.nix
           ./roles/laptop.nix
           ./roles/media.nix
