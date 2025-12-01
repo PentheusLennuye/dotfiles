394e9b9b1c40601b3dd629838592486213f51299
PentheusLennuye <gmc@cummings-online.ca>
2025-12-01 15:20:10 -0500

    logind no longer uses extraConfig

--- nixos/system/hosts/murasaki/hibernation.nix
+++ nixos/system/hosts/murasaki/hibernation.nix
@@ -3,11 +3,11 @@
 {
   services.logind = {
     lidSwitch = "suspend-then-hibernate";
-    settings.Login = ''
-      HandlePowerKey=suspend-then-hibernate
-      IdleAction=suspend-then-hibernate
-      IdleActionSec=2m
-    '';
+    settings.Login = {
+      HandlePowerKey = "suspend-then-hibernate";
+      IdleAction = "suspend-then-hibernate";
+      IdleActionSec = "2m";
+    };
   };
   systemd.sleep.extraConfig = "HibernateDelaySec=1h";
 }
