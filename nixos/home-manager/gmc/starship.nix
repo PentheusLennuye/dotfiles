15cdcea6cf0df440c5e5b438df0240ca0bf708a3
Cummings <gmc@cummings-online.ca>
2026-01-09 12:25:13 -0500

    Updated starship

--- nixos/home-manager/gmc/starship.nix
+++ nixos/home-manager/gmc/starship.nix
@@ -1,87 +1,71 @@
-{ pkgs, ... }:
+format = """
+[](fg:#a3aed2)\
+$os\
+[](bg:#769ff0 fg:#a3aed2)\
+$directory\
+[](fg:#769ff0 bg:#394260)\
+$nix_shell\
+$git_branch\
+$git_status\
+[](fg:#394260)\
+$package\
+\n$character"""
 
-{
+[directory]
+style = "fg:#e3e5e5 bg:#769ff0"
+format = "[ $path ]($style)"
+truncation_length = 3
+truncation_symbol = "…/"
 
-  programs.starship.enable = true;
-  programs.starship.settings = {
-    format =  "[░▒▓](#a3aed2)" +
-      "[  ](bg:#a3aed2 fg:#090c0c)" +
-      "[](bg:#769ff0 fg:#a3aed2)$hostname$directory" +
-      "[](fg:#769ff0 bg:#394260)$git_branch$git_status" +
-      "[](fg:#394260 bg:#212736)$c$golang$helm$kubernetes$lua$nix_shell$python$rust" +
-      "[](fg:#212736 bg:#1d2230)$time" +
-      "[](fg:#1d2230)\n$character";
-    directory = {
-      truncation_length = 3;
-      truncation_symbol = "…/";
-      format = "[$path]($style)";
-      style = "fg:#e3e5e5 bg:#769ff0";
-    };
-    directory.substitutions = {
-      "Documents" = "󰈙 ";
-      "Downloads" = " ";
-      "Music" = " ";
-      "Pictures" = " ";
-    };
-    git_branch = {
-      symbol = "";
-      style = "bg:#394260";
-      format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
-    };
-    git_status = {
-      style = "bg:#394260";
-      format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-    };
-    c = {
-      symbol = "";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    golang = {
-      symbol = "";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    helm = {
-      symbol = "⎈";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    hostname = {
-      format = "[ $hostname]($style)[:]($style)";
-      ssh_only = false;
-      style = "fg:#e3e5e5 bg:#769ff0";
-    };
-    kubernetes = {
-      symbol = "⎈";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    lua = {
-      symbol = "🌙";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    nix_shell = {
-      symbol = "";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    python = {
-      symbol = "🐍";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    rust = {
-      symbol = "🦀";
-      style = "bg:#212736";
-      format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-    };
-    time = {
-      disabled = false;
-      time_format = "%R"; # Hour:Minute Format
-      style = "bg:#1d2230";
-      format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
-    };
-  };
-}
+[directory.substitutions]
+"Documents" = "󰈙 "
+"Downloads" = " "
+"Music" = " "
+"Pictures" = " "
+
+[git_branch]
+symbol = ""
+style = "bg:#394260"
+format = '[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)'
+
+[git_status]
+style = "bg:#394260"
+format = '[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)'
+
+[nix_shell]
+format = "[[ $symbol](bg:#394260)]($style)"
+symbol = "❄️"
+style = "bg:394260"
+
+[nodejs]
+symbol = ""
+style = "bg:#212736"
+format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[rust]
+symbol = ""
+style = "bg:#212736"
+format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[golang]
+symbol = ""
+style = "bg:#212736"
+format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[os]
+disabled = false
+format = "[[$symbol](bg:#a3aed2)]($style)"
+style = "bg:#a3aed2"
+
+[package]
+format = "[$symbol$version](208)"
+#[php]
+#symbol = ""
+#style = "bg:#212736"
+#format = '[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)'
+
+[time]
+disabled = true
+time_format = "%R" # Hour:Minute Format
+style = "bg:#1d2230"
+format = '[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)'
