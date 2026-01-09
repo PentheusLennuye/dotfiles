e8718ad6edd120e32034909af2fe3543efde382d
Cummings <gmc@cummings-online.ca>
2026-01-09 12:28:31 -0500

    Added the nd command for nix develop

--- nixos/home-manager/gmc/zsh.nix
+++ nixos/home-manager/gmc/zsh.nix
@@ -6,20 +6,22 @@
     enable = true;
     autosuggestion.enable = false;
     initContent = ''
-        bindkey "$terminfo[kcuu1]" history-substring-search-up
-        bindkey "$terminfo[kcud1]" history-substring-search-down
-        export LIBVIRT_DEFAULT_URI="qemu:///system";
-        source <(kubectl completion zsh)
+      bindkey "$terminfo[kcuu1]" history-substring-search-up
+      bindkey "$terminfo[kcud1]" history-substring-search-down
+      export LIBVIRT_DEFAULT_URI="qemu:///system";
+      source <(kubectl completion zsh)
     '';
-    plugins = [{
-      name = "zsh-history-substring-search";
-      src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-history-substring-search";
-        rev = "v1.1.0";
-        sha256 = "GSEvgvgWi1rrsgikTzDXokHTROoyPRlU0FVpAoEmXG4=";
-      };
-    }];
+    plugins = [
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "v1.1.0";
+          sha256 = "GSEvgvgWi1rrsgikTzDXokHTROoyPRlU0FVpAoEmXG4=";
+        };
+      }
+    ];
     sessionVariables = {
       CA = "$HOME/spaces/tech/infra/ca";
       DF = "$HOME/spaces/tech/infra/dotfiles";
@@ -29,21 +31,20 @@
       SAP = "$HOME/spaces/sap/projects/src";
       SSH_ASKPASS = "";
       SRC = "$HOME/spaces/tech/programming/projects";
-      STEAM_EXTRA_COMPAT_TOOLS_PATH =
-        "\\\${HOME}/.steam/root/compatibilitytools.d";
+      STEAM_EXTRA_COMPAT_TOOLS_PATH = "\\\${HOME}/.steam/root/compatibilitytools.d";
     };
     shellAliases = {
-      hbs="home-manager build switch";
-      k="kubectl";
-      ke="kubectl exec";
-      ls="lsd";
-      ll="ls -l";
-      la="ls -la";
-      ssh="TERM=xterm-256color ssh";
-      vsh="TERM=xterm-256color vagrant ssh";
-      sns="sudo nixos-rebuild switch";
-      snt="sudo nixos-rebuild test";
+      hbs = "home-manager build switch";
+      k = "kubectl";
+      ke = "kubectl exec";
+      ls = "lsd";
+      ll = "ls -l";
+      la = "ls -la";
+      nd = "nix develop path:.nix --command zsh";
+      ssh = "TERM=xterm-256color ssh";
+      vsh = "TERM=xterm-256color vagrant ssh";
+      sns = "sudo nixos-rebuild switch";
+      snt = "sudo nixos-rebuild test";
     };
   };
 }
-
