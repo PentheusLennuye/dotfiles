0a4a02ba1adcf603ee6c9bbfadd598bf43f48b7a
PentheusLennuye <gmc@cummings-online.ca>
2026-02-22 18:23:02 -0500

    Moving nixos binary cache to Lupin

--- nixos/system/roles/binarycache.nix
+++ nixos/system/roles/binarycache.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   networking.firewall.allowedTCPPorts = [
@@ -10,7 +10,7 @@
       recommendedProxySettings = true;
       virtualHosts = {
         "nixoscache.cummings-online.local" = {
-            locations."/".proxyPass =  "http://127.0.0.1:${toString config.services.nix-serve.port}";
+          locations."/".proxyPass = "http://127.0.0.1:${toString config.services.nix-serve.port}";
         };
       };
     };
@@ -22,4 +22,3 @@
     };
   };
 }
-
