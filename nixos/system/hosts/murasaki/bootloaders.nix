56ba2d2adf64a31cad7bbaf0ae0c2b18ce9559f6
PentheusLennuye <gmc@cummings-online.ca>
2026-02-22 20:22:00 -0500

    Enabling NFS

--- nixos/system/hosts/murasaki/bootloaders.nix
+++ nixos/system/hosts/murasaki/bootloaders.nix
@@ -2,25 +2,47 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   boot = {
     extraModulePackages = [ ];
     initrd = {
-      availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
-      kernelModules = [ "kvm-intel" "nfs" "nfs4"];
-      supportedFilesystems = [ "nfs" "nfs4" ];
+      availableKernelModules = [
+        "xhci_pci"
+        "thunderbolt"
+        "nvme"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+        "sdhci_pci"
+      ];
+      kernelModules = [
+        "kvm-intel"
+        "nfs"
+        "nfs4"
+      ];
+      supportedFilesystems = [
+        "nfs"
+        "nfs4"
+      ];
     };
-    kernelModules = ["kvm-intel" "nfs" "nfs4" ];
-    kernelParams = [ "mem_sleep_default=deep" "resume_offset=0" ];
+    kernelModules = [
+      "kvm-intel"
+      "nfs"
+      "nfs4"
+    ];
+    kernelParams = [
+      "mem_sleep_default=deep"
+      "resume_offset=0"
+    ];
     loader = {
-        efi = {
-            canTouchEfiVariables = true;
-        };
-        systemd-boot = {
-            enable = true;
-        };
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      systemd-boot = {
+        enable = true;
+      };
     };
   };
 }
--- nixos/system/modules/common_packages.nix
+++ nixos/system/modules/common_packages.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
 
@@ -20,6 +20,7 @@
     lsof
     lshw
     mesa
+    nfs-utils
     openssl
     pciutils
     pstree
