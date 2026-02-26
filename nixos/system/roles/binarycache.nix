516de877a3082e77672b71102aad892eacb984c2
PentheusLennuye <gmc@cummings-online.ca>
2026-02-26 13:50:43 -0500

    Local binary cache adding ssh enable for updates

--- nixos/system/roles/binarycache.nix
+++ nixos/system/roles/binarycache.nix
@@ -4,6 +4,19 @@
   networking.firewall.allowedTCPPorts = [
     5000
   ];
+  nix.sshServe = {
+    enable = true;
+    keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXtgtsAG7w5Jkry0wScULHUo/O8SMQtipylA7ap3lDN ncu2025@cummings-online.ca"
+    ];
+  };
+  users = {
+    users.ncu = {
+      description = "NixOS Cache Updater";
+      isNormalUser = false;
+      useDefaultShell = true;
+    };
+  };
   services = {
     nix-serve = {
       bindAddress = "0.0.0.0";
