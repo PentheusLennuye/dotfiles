7a831665886b0e42abfacd5837722a11082b1e0c
PentheusLennuye <gmc@cummings-online.ca>
2026-03-01 13:36:50 -0500

    Kitty uses meta-c.

--- nixos/system/modules/gmc-wants.nix
+++ nixos/system/modules/gmc-wants.nix
@@ -1,46 +1,45 @@
-{ config, pkgs, ... }:
-
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
-    _1password-gui                         # 1Password password manager
-    anki                                   # Flashcards, requires QT
-    appimage-run                           # Needed for Cider 2
-    calibre                                # eBook software
+    _1password-gui # 1Password password manager
+    bitwarden-desktop # Password manager
+    brave # Browser
+    appimage-run # Needed for Cider 2
+    calibre # eBook software
     canon-cups-ufr2
-    dexed                                  # DX7 VST plugin
+    dexed # DX7 VST plugin
     digikam
     firefox
-    freetube                               # YouTube on chromium
+    freetube # YouTube on chromium
     gimp
-    mesa-demos                             # Testing glx info
+    mesa-demos # Testing glx info
     openpomodoro-cli
-    grim                                   # Wayland image grabber
+    grim # Wayland image grabber
     hunspell
     hunspellDicts.de-de
     hunspellDicts.en_CA-large
     hunspellDicts.es_MX
     hunspellDicts.fr-any
-    hyprpaper                              # wallpaper for Hyprland
+    hyprpaper # wallpaper for Hyprland
     inkscape
     joplin-desktop
-    keymapp                                # flashing onyx keyboards
+    keymapp # flashing onyx keyboards
     kitty
-    krita                                  # Painting app
+    krita # Painting app
     obsidian
     onlyoffice-desktopeditors
-    pavucontrol                            # PulseAudio control requires GTK
-    reaper                                 # DAW
-    screenfetch                            # Show off!
-    slurp                                  # Hyprland screen selector
-    starship
-    surge                                  # synthesizer
-    swaynotificationcenter                 # Waybar modules require this
-    thunderbird
-    vista-fonts                             # Better fonts for Firefox etc
-    waybar                                 # task/toolbar for Hyprland
-    wofi                                   # program selector for Hyprland
-    zotero                                 # reference manager
+    pavucontrol # PulseAudio control requires GTK
+    reaper # DAW
+    screenfetch # Show off!
+    slurp # Hyprland screen selector
+    starship # CLI ricer
+    surge # synthesizer
+    swaynotificationcenter # Waybar modules require this
+    thunderbird # Email
+    vista-fonts # Better fonts for Firefox etc
+    waybar # task/toolbar for Hyprland
+    wofi # program selector for Hyprland
+    zotero # reference manager
   ];
 }
-
