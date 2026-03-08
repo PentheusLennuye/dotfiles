42d8d2dea355788fcd137b14182697f1ef217c4d
PentheusLennuye <gmc@cummings-online.ca>
2026-03-07 23:10:57 -0500

    Kernel hardened

--- nixos/system/modules/common_security.nix
+++ nixos/system/modules/common_security.nix
@@ -117,6 +117,26 @@
   #  │                                                                                          │
   #  └──────────────────────────────────────────────────────────────────────────────────────────┘
 
+  boot.kernel.sysctl = {
+    "dev.tty.ldisc_autoload" = 0;
+    "fs.protected_fifos" = 2;
+    "fs.protected_regular" = 2;
+    "fs.suid_dumpable" = 0;
+    "kernel.kptr_restrict" = 2;
+    "kernel.modules_disabled" = 1;
+    "kernel.sysrq" = 0;
+    "kernel.unprivileged_bpf_disabled" = 1;
+    "net.core.bpf_jit_harden" = 2;
+    "net.ipv4.conf.all.forwarding" = 0;
+    "net.ipv4.conf.all.log_martians" = 1;
+    "net.ipv4.conf.all.rp_filter" = 1;
+    "net.ipv4.conf.all.send_redirects" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
+    "net.ipv4.conf.default.log_martians" = 1;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+  };
+
   boot.blacklistedKernelModules = [
     "dccp"
     "sctp"
@@ -142,7 +162,7 @@
   };
 
   services.openssh = {
-    ports = [ 22 ];
+    ports = [ 2222 ];
     settings = {
       AllowAgentForwarding = false;
       AllowTCPForwarding = false;
