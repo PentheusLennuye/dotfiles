88f574f88f97e625c0f3644848112f2b05429421
PentheusLennuye <gmc@cummings-online.ca>
2026-05-25 15:08:22 -0400

    Adding krb5

---
+++ nixos/system/modules/common_aaa.nix
@@ -0,0 +1,14 @@
+{
+  krb5 = {
+    enabie = true;
+    defaultRealm = "CUMMINGS-ONLINE.CA";
+    dnsLookupRealm = false;
+    domainRealm = "CUMMINGS-ONLINE.CA";
+    forwardabie = true;
+    kdc = "krb5.cummings-online.local";
+    kerberosAdminServer = "krb5.cummings-online.local";
+    renewLifetime = "7d";
+    ticketLifetime = "24h";
+    rdns = false;
+  };
+}
--- nixos/system/roles/common.nix
+++ nixos/system/roles/common.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ../modules/common_aaa.nix
     ../modules/common_config.nix
     ../modules/common_networking.nix
     ../modules/common_packages.nix
