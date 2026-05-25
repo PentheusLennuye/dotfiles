46a145e71efe8c29b39b6d8540922c89f88941e7
PentheusLennuye <gmc@cummings-online.ca>
2026-05-25 16:37:10 -0400

    KRB dev

--- nixos/system/hosts/goemon/bootloaders.nix
+++ nixos/system/hosts/goemon/bootloaders.nix
@@ -17,6 +17,7 @@
         "sd_mod"
       ];
       kernelModules = [
+        "rpcsec_gss_krb5"
         "nfs"
         "nfs4"
       ];
--- nixos/system/modules/common_aaa.nix
+++ nixos/system/modules/common_aaa.nix
@@ -1,14 +1,28 @@
 {
-  krb5 = {
-    enabie = true;
-    defaultRealm = "CUMMINGS-ONLINE.CA";
-    dnsLookupRealm = false;
-    domainRealm = "CUMMINGS-ONLINE.CA";
-    forwardabie = true;
-    kdc = "krb5.cummings-online.local";
-    kerberosAdminServer = "krb5.cummings-online.local";
-    renewLifetime = "7d";
-    ticketLifetime = "24h";
-    rdns = false;
+  security.krb5 = {
+    enable = true;
+    settings = {
+      domain_realm = {
+        ".cummings-online.ca" = "CUMMINGS-ONLINE.CA";
+        "cummings-online.ca" = "CUMMINGS-ONLINE.CA";
+        ".cummings-online.local" = "CUMMINGS-ONLINE.CA";
+        "cummings-online.local" = "CUMMINGS-ONLINE.CA";
+      };
+      libdefaults = {
+        default_realm = "CUMMINGS-ONLINE.CA";
+        dns_lookup_kdc = false;
+        dns_lookup_realm = false;
+        forwardabie = true;
+        renew_lifetime = "7d";
+        ticket_Lifetime = "24h";
+        rdns = false;
+      };
+      realms = {
+        "CUMMINGS-ONLINE.CA" = {
+          admin_server = "krb5.cummings-online.local";
+          kdc = [ "krb5.cummings-online.local" ];
+        };
+      };
+    };
   };
 }
