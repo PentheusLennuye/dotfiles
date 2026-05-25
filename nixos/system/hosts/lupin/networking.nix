7f74b9206e4138e19015f78cb2f99013f658415d
PentheusLennuye <gmc@cummings-online.ca>
2026-05-25 17:44:56 -0400

    path missing relative marker

--- nixos/system/hosts/lupin/networking.nix
+++ nixos/system/hosts/lupin/networking.nix
@@ -1,4 +1,7 @@
-{ config, pkgs, lib, ... }:
+{
+  lib,
+  ...
+}:
 
 {
   networking = {
@@ -10,12 +13,22 @@
     interfaces = {
       eno1 = {
         ipv4 = {
-          addresses = [{ address = "172.16.73.2"; prefixLength = 24; }];
+          addresses = [
+            {
+              address = "172.16.73.2";
+              prefixLength = 24;
+            }
+          ];
         };
       };
       enp0s20f0u10 = {
         ipv4 = {
-          addresses = [{ address = "10.11.0.2"; prefixLength = 16; }];
+          addresses = [
+            {
+              address = "10.11.0.2";
+              prefixLength = 16;
+            }
+          ];
         };
       };
     };
@@ -23,8 +36,5 @@
 
   services.openssh = {
     enable = true;
-    settings = {
-        PasswordAuthentication = true;
-    };
   };
 }
