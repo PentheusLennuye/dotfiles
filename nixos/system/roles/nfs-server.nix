2ef85889286d53249b85de334ac4e8ba66846d52
PentheusLennuye <gmc@cummings-online.ca>
2026-05-26 12:19:18 -0400

    Found nfs problem

--- nixos/system/roles/nfs-server.nix
+++ nixos/system/roles/nfs-server.nix
@@ -3,27 +3,26 @@
 {
   systemd.tmpfiles.rules = [
     "d /export 0755 nobody nogroup - "
+    "d /export/private 0755 nobody nogroup - "
+    "d /export/protected 0755 nobody nogroup - "
     "d /srv 0755 nobody nogroup -"
+    "d /srv/home 0755 nobody nogroup -"
     "d /srv/fleetwood 0755 nobody nogroup - "
-    "d /srv/home 0755 nobody nogroup - "
   ];
 
-  fileSystems."/export/fleetwood" = {
+  fileSystems."/export/protected/fleetwood" = {
     device = "/srv/fleetwood";
     options = [ "bind" ];
   };
 
-  fileSystems."/export/home" = {
+  fileSystems."/export/private/home" = {
     device = "/srv/home";
     options = [ "bind" ];
   };
 
-  fileSystems."/export/backup" = {
-    device = "/srv/backup";
-    options = [ "bind" ];
-  };
-
-  networking.firewall.allowedTCPPorts = [ 2049 ];
+  networking.firewall.allowedTCPPorts = [
+    2049
+  ];
 
   services.nfs = {
     settings.nfsd = {
@@ -36,11 +35,11 @@
     };
     server = {
       enable = true;
+      # fsid means "this is the root of the filesystem"
       exports = ''
         /export 172.16.73.0/24(rw,fsid=0,no_subtree_check) 192.168.68.0/24(rw,fsid=0,no_subtree_check) 192.168.73.0/24(rw,fsid=0,no_subtree_check) 10.0.0.0/24(rw,fsid=0,no_subtree_check) 10.11.0.0/16(rw,fsid=0,no_subtree_check)
-        /export/fleetwood 172.16.73.0/24(rw,insecure,no_subtree_check)
-        /export/backup 172.16.73.0/24(rw,insecure,no_subtree_check) 192.168.68.0/24(rw,insecure,no_subtree_check)
-        /export/home *(rw,insecure,no_subtree_check,sec=krb5i)
+        /export/protected 172.16.73.0/24(rw,insecure,no_subtree_check)
+        /export/private *(rw,no_root_squash,sec=krb5)
       '';
     };
   };
