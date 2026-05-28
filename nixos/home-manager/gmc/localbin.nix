e145b8e0e7ff273d7e974cf9c925fe55d7d4dbf3
PentheusLennuye <gmc@cummings-online.ca>
2026-05-27 21:43:24 -0400

    Obsidian sync script

--- nixos/home-manager/gmc/localbin.nix
+++ nixos/home-manager/gmc/localbin.nix
@@ -1,13 +1,10 @@
-{ pkgs, config, xdg, ... }:
-
-
 {
   home.file = {
     ".local/bin/backup_home".source = bin/backup_home;
     ".local/bin/backup_home_small".source = bin/backup_home_small;
     ".local/bin/restore_home".source = bin/restore_home;
     ".local/bin/restore_home_small".source = bin/restore_home_small;
-    ".local/bin/push_vault".source = bin/push_vault.sh;
-    ".local/bin/pull_vault".source = bin/pull_vault.sh;
+    ".local/bin/pkm_sync_in".source = bin/pkm_sync_in.sh;
+    ".local/bin/pkm_sync_out".source = bin/pkm_sync_out.sh;
   };
 }
