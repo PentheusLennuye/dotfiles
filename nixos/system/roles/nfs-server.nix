8d441f78107b85daf18289dd4fd4965e623b420a
PentheusLennuye <gmc@cummings-online.ca>
2026-06-12 22:23:18 -0400

    Jigen is no longer NFS client

--- nixos/system/roles/nfs-server.nix
+++ nixos/system/roles/nfs-server.nix
@@ -11,7 +11,7 @@
       enable = true;
       # fsid means "this is the root of the filesystem"
       exports = ''
-        /srv/nfs *(rw,sync,fsid=0,crossmnt,no_subtree_check,sec=krb5:sys)
+        /srv/nfs *(rw,insecure,sync,fsid=0,crossmnt,no_subtree_check,sec=krb5:sys,no_root_squash)
         /srv/nfs/private *(rw,sync,no_subtree_check,sec=krb5)
         /srv/nfs/protected 10.11.0.0/16(rw,insecure,sync,no_subtree_check,no_root_squash) 172.16.73.0/24(rw,insecure,sync,no_subtree_check,no_root_squash)
         /srv/nfs/public *(rw,wdelay,no_subtree_check)
