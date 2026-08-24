57db37d0b8eb00bec59ef763831b913edbdcb704
PentheusLennuye <gmc@cummings-online.ca>
2026-08-24 17:09:37 -0400

    Eliminated openldap from fleetwood; using 389ds

--- k8s/fleetwood/aaa/openldap/client_config/linux/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install sssd-ldap ldap-utils
-
-sudo echo "session optional pam_mkhomedir.so" >> /etc/pam.d/common-session
-
-sudo cp cummings-online.crt /usr/local/share/ca-certificates/
-sudo update-ca-certificates
-
