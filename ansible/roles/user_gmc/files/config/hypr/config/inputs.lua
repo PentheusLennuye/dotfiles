be428380c8524aebd291abde06ee73fe52af9219
PentheusLennuye <gmc@cummings-online.ca>
2026-09-20 15:53:26 -0400

    Added hyprgrass

--- ansible/roles/user_gmc/files/config/hypr/config/inputs.lua
+++ ansible/roles/user_gmc/files/config/hypr/config/inputs.lua
@@ -27,11 +27,23 @@ hl.gesture({ fingers = 3, direction = "down", action = "close" })
 hl.gesture({ fingers = 3, direction = "up", mods = "SUPER", action = "fullscreen" })
 hl.gesture({ fingers = 3, direction = "left", action = "float" })
 
--- Onscreen keyboard
--- hl.gesture({
---  	fingers = 3,
---  	direction = "up",
---  	action = function()
--- 		hl.exec_cmd("pkill wf-osk || wf-osk")
--- 	end,
--- })
+-- Onscreen keyboard ───────────────────────────────────────────3
+hl.plugin.hyprgrass.bind({
+	pattern = {
+		direction = "u",
+		fingers = 1,
+		kind = "edge",
+		origin = "d",
+	},
+	action = hl.dsp.exec_cmd("wf-osk --height 240 --width 800 -a bottom"),
+})
+
+hl.plugin.hyprgrass.bind({
+	pattern = {
+		direction = "d",
+		fingers = 1,
+		kind = "edge",
+		origin = "u",
+	},
+	action = hl.dsp.exec_cmd("pkill wf-osk"),
+})
